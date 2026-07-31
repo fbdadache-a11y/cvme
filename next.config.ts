29af16efc545c11793de30d1cc4ac462833a72db
Dadache Fouad <fbdadache@gmail.com>
2026-07-31 06:33:22 +0100

    Configure basePath and assetPrefix for GitHub Actions
    Add GitHub Actions support for base path and asset prefix.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
+  // إعداداتك الأصلية
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  // الإعدادات الجديدة لإصلاح مشكلة GitHub Pages
+  basePath: isGithubActions ? "/cvme" : "",
+  assetPrefix: isGithubActions ? "/cvme/" : "",
 };
 
 export default nextConfig;
