505084fc900187479ce5b4e2b69fbfe5fbbd274d
Dadache Fouad <fbdadache@gmail.com>
2026-07-31 05:34:15 +0100

    Refactor metadata type and update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
+import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Fouad Dadache",
-  description: "Personal Portfolio & CV",
+  description: "Simple CV and portfolio"
 };
 
 export default function RootLayout({
-  children,
-}: {
+  children
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
