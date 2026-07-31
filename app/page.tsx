f6c00a4d01e2b333bb8ec267b21d8eebaf679131
Dadache Fouad <fbdadache@gmail.com>
2026-07-31 06:43:10 +0100

    Add Work component to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Work from "./components/Work";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
+
       <main>
         <Hero />
+        <Work />
       </main>
     </>
   );
