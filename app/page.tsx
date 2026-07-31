e86453691a2495f2a66bb8850aa9d3ec2a0966bb
Dadache Fouad <fbdadache@gmail.com>
2026-07-31 05:13:05 +0100

    Create Home component with CV download link

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Fouad Dadache</h1>
+      <p>Management Student</p>
+
+      <a href="/cv.pdf" target="_blank">
+        Download CV
+      </a>
+    </main>
+  );
+}
